bbef4d66e92d3e541db1326678d10b51dcd917a0
joshuakaisar <kaisarjoshua@gmail.com>
2022-03-08 12:59:16 -0500

    attempt nav 2

--- tests/homepage_test.py
+++
@@ -1,7 +0,0 @@
-"""This test the homepage"""
-
-
-def test_request_example(client):
-    """This makes the index page"""
-    response = client.get("/")
-    assert b"Hello, welcome to my website for Project 1" in response.data
